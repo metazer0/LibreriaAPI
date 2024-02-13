15a19089b8752a36b86f5980112d88a2ccf782f0
Julio David <isaacdvid15.96@outlook.com>
2024-02-13 11:12:12 -0600

    Add project files.

---
+++ Libreria/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Libreria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libreria.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+               
+        }
+
+        DbSet<Libro> Libros { get; set; }
+        DbSet<Autor> Autores { get; set; }
+    }
+}
---
+++ Libreria/Models/Libro.cs
@@ -0,0 +1,10 @@
+﻿namespace Libreria.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string? NombreLibro { get; set; }
+        public bool Retirado { get; set; }
+        public List<Autor>? Autores { get; set; }
+    }
+}
